78e5b213bfa09ce0bb4a5b7164e98af25e675038
Shannon <sp358@njit.edu>
2019-12-08 23:01:31 -0500

    Fix:corrected errors with spacing and references

--- blog_api/views/UserView.py
+++ blog_api/views/UserView.py
@@ -1,4 +1,4 @@
-from flask import request, json, Response, Blueprint
+from flask import request, json, Response, Blueprint, g
 from ..models.UserModel import UserModel, UserSchema
 from ..shared.Authentication import Auth
 
@@ -29,12 +29,13 @@ def create():
 
     return custom_response({'jwt_token': token}, 201)
 
-@user_api.route('/',methods=['GET'])
+
+@user_api.route('/', methods=['GET'])
 @Auth.auth_required
 def get_all():
     users = UserModel.get_all_users()
     ser_users = user_schema.dump(users, many=True).data
-    return custom_response((ser_users, 200))
+    return custom_response(ser_users, 200)
 
 
 @user_api.route('/login', methods=['Post'])
@@ -46,7 +47,7 @@ def login():
         return custom_response(error, 400)
 
     if not data.get('email') or not data.get('password'):
-        return custom_response({'error': 'you need email and password to sign in'},400)
+        return custom_response({'error': 'you need email and password to sign in'}, 400)
 
     user = UserModel.get_user_by_email(data.get('email'))
 
@@ -54,13 +55,56 @@ def login():
         return custom_response({'error': 'invalid credentials'}, 400)
 
     if not user.check_hash(data.get('password')):
-        return custom_response({'error': 'invalid credentials'},400)
+        return custom_response({'error': 'invalid credentials'}, 400)
 
-    ser_data = user_schema(user).data
+    ser_data = user_schema.dump(user).data
 
     token = Auth.generate_token(ser_data.get('id'))
 
-    return custom_response({'jwt_token':token},200)
+    return custom_response({'jwt_token': token}, 200)
+
+
+@user_api.route('/<int:user_id>', methods=['GET'])
+@Auth.auth_required
+def get_a_user(user_id):
+    user = UserModel.get_one_user(user_id)
+
+    if not user:
+        return custom_response({'error': 'user not found'}, 404)
+
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['PUT'])
+@Auth.auth_required
+def update():
+    req_data = request.get_json()
+    data, error = user_schema.laod(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.update(data)
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
+
+@user_api.route('/me', methods=['Delete'])
+@Auth.auth_required
+def delete():
+    user = UserModel.get_one_user(g.user.get('id'))
+    user.delete()
+    return custom_response({'message': 'deleted'}, 204)
+
+
+@user_api.route('/me', methods=['GET'])
+@Auth.auth_required
+def get_me():
+    user = UserModel.get_one_user(g.user.get('id'))
+    ser_user = user_schema.dump(user).data
+    return custom_response(ser_user, 200)
+
 
 def custom_response(res, status_code):
     return Response(
@@ -68,47 +112,3 @@ def custom_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
-
-@user_api.route('/<int:user_id>', methods=['GET'])
-@Auth.auth_required
-def get_a_user(user_id):
-
-    user = UserModel.get_one_user(user_id)
-
-    if not user:
-        return custom_response({'error': 'user not found'}, 404)
-
-    ser_user = user_schema.dump(user).data
-    return custom_response(ser_user,200)
-
-@user_api.route('/me', methods=['PUT'])
-@Auth.auth_required
-def update():
-
-    req_data = request.get_json()
-    data,error = user_schema.laod(req_data, partial=True)
-    if error:
-        return custom_response(error, 400)
-
-    user = UserModel.get_one_user(g.user.get('id'))
-    user.update(data)
-    ser_user = user_schema.dump(user).data
-    return custom_response(ser_user)
-
-@user_api.route('/me', methods=['Delete'])
-@Auth.auth_required
-def delete():
-
-    user = UserModel.get_one_user(g.user.get('id'))
-    user.delete()
-    return custom_response({'message': 'deleted'}, 204)
-
-@user_api.route('/me', methods=['GET'])
-@Auth.auth_required
-def get_me():
-
-    user = UserModel.get_one_user(g.user.get('id'))
-    ser_user = user_schema.dump(user).data
-    return custom_response(ser_user, 200)
-
-
