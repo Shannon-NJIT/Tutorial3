ed0c49bb09b7f12b87c75c51cf83ae732eb4f034
Shannon <sp358@njit.edu>
2019-12-08 21:25:25 -0500

    Task: added auth decorator

--- blog_api/shared/Authentication.py
+++ blog_api/shared/Authentication.py
@@ -44,5 +44,39 @@ class Auth():
                  re['error'] = {'message': 'Ivalid token, please try again with a new token'}
                  return re
 
+        @staticmethod
+        def auth_required(func):
 
-        
+            @wraps(func)
+            def decorated_auth(*args, **kwargs):
+                if 'api-token' not in request.headers:
+                    return Response(
+                        mimetype="application/json",
+                        response=json.dumps(
+                            {'error': 'Authentication token is not available, please login to get one'}),
+                        status=400
+                    )
+
+                token = request.headers.get('api.token')
+                data = Auth.decode_token(token)
+                if data['error']:
+                    return Response(
+                        mimetype="application/json",
+                        response=json.dumps(data['error']),
+                        status=400
+                    )
+
+            user_id = data['data']['user_id']
+            check_user = UserModel.get_one_user(user_id)
+
+            if not check_user:
+                return Response(
+                    mimetype="application/json",
+                    response=json.dumps({'error': 'users does not exist, invalid token'}),
+                    status=400
+                )
+
+            g.user = {'id': user_id}
+            return func(*args, **kwargs)
+
+        return decorated_auth
