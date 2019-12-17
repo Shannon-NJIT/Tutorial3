0699526b4a51c3764fc9e82b9500e164c63a6c47
Shannon <sp358@njit.edu>
2019-12-16 21:56:14 -0500

    Fix: updated with renamed db file

--- Database/database.py
+++ Database/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, Session
 
-engine = create_engine('sqlite:////web/Sqlite-Data/cat.db')
+engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 
 model = declarative_base()
 
